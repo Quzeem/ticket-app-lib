73c02f6515a530dd55652dfa1215e1ab3b9a9a20
Quzeem <quzeemagbaje@gmail.com>
2021-07-16 17:42:40 +0100

    Updates

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ export * from './errors/notAuthenticatedError';
 export * from './errors/notFoundError';
 export * from './errors/requestValidationError';
 
-export * from './middlewares/request-validators/loginValidator';
-export * from './middlewares/request-validators/signupValidator';
 export * from './middlewares/errorHandler';
 export * from './middlewares/handleValidationError';
 export * from './middlewares/isLoggedIn';
